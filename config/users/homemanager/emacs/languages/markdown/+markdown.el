692bae85026eabff44f6859c3ddab11af80cd379
IllustratedMan-code <davidalewis00@gmail.com>
2025-05-13 20:23:11 -0400

    fixed markdown viewing

--- config/users/homemanager/emacs/languages/markdown/+markdown.el
+++ config/users/homemanager/emacs/languages/markdown/+markdown.el
@@ -9,30 +9,52 @@
 		(defconst markdown-language-keyword-properties
 			'(face markdown-language-keyword-face ))
 		(defun nb/unhide-current-line (limit)
-		"Font-lock function"
-				(let ((start (max (point) (car nb/current-line)))
-								(end (min limit (cdr nb/current-line))))
-						(when (< start end)
-						(remove-text-properties start end
-														'(invisible t display "" composition ""))
-						(goto-char limit)
-						t)))
+		  "Font-lock function"
+		  (let ((start (max (point) (car nb/current-line)))
+			(end (min limit (cdr nb/current-line))))
+		    (when (< start end)
+		      (remove-text-properties start end
+					      '(invisible t display "" composition ""))
+		      (goto-char limit)
+		      t)))
 
 		(defun nb/refontify-on-linemove ()
-		"Post-command-hook"
-				(let* ((start (line-beginning-position))
-								(end (line-beginning-position 2))
-								(needs-update (not (equal start (car nb/current-line)))))
-						(setq nb/current-line (cons start end))
-						(when needs-update
-						(font-lock-fontify-block 3))))
+		  "Post-command-hook"
+		  (let* ((start (line-beginning-position))
+			 (end (line-beginning-position 2))
+			 (needs-update (not (equal start (car nb/current-line)))))
+		    (setq nb/current-line (cons start end))
+		    (when needs-update
+		      (font-lock-fontify-block 3))))
 
+		(defun nb/unhide-fences (limit)
+		  "Unhide code block fences ``` in markdown."
+		  (when (re-search-forward "^\\([[:space:]]*\\(```\\)+.*\\)\\(\n\\)?" limit t)
+		    (remove-text-properties (match-beginning 0) (match-end 0)
+					    '(invisible t display "" composition ""))
+		    t))
+
+		(defun nb/unhide-yaml-fence-if-block (limit)
+		  "Unhide YAML fences only if they enclose at least one key-value pair."
+		  (when (re-search-forward "^---\n\\(.+?\\)\n---\\(\n\\|\\'\\)" limit t)
+		    ;; Unhide the entire fenced block if non-empty content exists.
+		    (remove-text-properties (match-beginning 0) (match-end 0)
+					    '(invisible t display "" composition ""))
+		    ;; Remove fontification inside YAML content
+		    (let ((content-start (match-beginning 0))
+			  (content-end (match-end 0)))
+		      (remove-text-properties content-start content-end
+					      '(face nil invisible nil display nil composition nil)))
+		    t))
+		
 		(defun nb/markdown-unhighlight ()
-		"Enable markdown concealling"
-				(interactive)
-				(markdown-toggle-markup-hiding 'toggle)
-				(font-lock-add-keywords nil '((nb/unhide-current-line)) t)
-				(add-hook 'post-command-hook #'nb/refontify-on-linemove nil t))
+		  "Enable markdown concealling"
+		  (interactive)
+		  (markdown-toggle-markup-hiding 'toggle)
+		  (font-lock-add-keywords nil '((nb/unhide-fences)
+						(nb/unhide-yaml-fence-if-block)
+						(nb/unhide-current-line)) t)
+		  (add-hook 'post-command-hook #'nb/refontify-on-linemove nil t))
 		(require 'base16-stylix-theme)
 		(custom-set-faces
 		 `(markdown-code-face ((t (:background ,(plist-get base16-stylix-theme-colors :base01) :extend t)))))
@@ -42,9 +64,11 @@
 
 (use-package apheleia
 	:defer t
-	:hook (gfm-mode . apheleia-mode)
+	:hook ((markdown-mode . apheleia-mode)
+	       (gfm-mode . apheleia-mode))
 	:config
-	(add-to-list 'apheleia-mode-alist '(gfm-mode . prettier-markdown))
+	(add-to-list 'apheleia-mode-alist '(gfm-mode . prettier))
+	(add-to-list 'apheleia-mode-alist '(markdown-mode . prettier))
 	:general
 	(local-leader-definer
 		;; markdown-mode won't work here
