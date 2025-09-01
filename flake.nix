a33e24c7d505940c6262e3142fce4c7f8faed189
IllustratedMan-code <davidalewis00@gmail.com>
2025-09-01 16:19:33 -0400

    adds usb config/laptop config

--- flake.nix
+++ flake.nix
@@ -54,12 +54,12 @@
       nixosConfigurations."davidnix" = nixpkgs.lib.nixosSystem {
         inherit system;
         inherit specialArgs;
-        modules =  modules ++ [ .config/hardware-configuration.nix ];
+        modules =  modules ++ [ ./config/hardware-configuration.nix ];
       };
       nixosConfigurations."davidnixusb" = nixpkgs.lib.nixosSystem {
         inherit system;
         inherit specialArgs;
-        modules =  modules ++ [ .config/usb-hardware-config.nix ];
+        modules =  modules ++ [ ./config/usb-hardware-config.nix ];
       };
       packages.${system}.iso = self.nixosConfigurations.iso.config.system.build.isoImage;
     };
